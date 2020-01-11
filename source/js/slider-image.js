c05de72a698e45fa8da5c4b129c1ad493fad1ab7
srg <skabass@yandex.ru>
2020-01-12 02:10:11 +0300

    Оптимизация кроссбраузерности, добавление полифила для IE11 для тега picture, добавление crossorigin для загрузки шрифтов, добавление js для мобильной версии слайдера, исправления для PP

---
+++ source/js/slider-image.js
@@ -0,0 +1,18 @@
+if (window.innerWidth < 768) {
+  var buttonBefore = document.querySelector('.control__button--before');
+  var buttonAfter = document.querySelector('.control__button--after');
+  var imageBefore = document.querySelector('.slider-image__item--before');
+  var imageAfter = document.querySelector('.slider-image__item--after');
+  var control = document.querySelector('.control__mobile-track');
+
+  buttonBefore.addEventListener('click', function () {
+    imageAfter.style.display = 'none';
+    imageBefore.style.display = 'block';
+    control.style.paddingLeft = '6px';
+  });
+  buttonAfter.addEventListener('click', function () {
+    imageBefore.style.display = 'none';
+    imageAfter.style.display = 'block';
+    control.style.paddingLeft = '42px';
+  });
+}
